18e7f7203691591079abe45006380e0418097c2e
davidandblake <davidfrieder@gmail.com>
2021-02-14 17:36:11 -0800

    This is being pushed to Github so David can look at it.
    This is a program that creates a client and server. The program isn't running all the way through, as I have found by placing sout commands at verious points in the program. David is going to help me figure out what is wrong with it.

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -3,27 +3,37 @@ package _01_Intro_To_Sockets.client;
 import java.net.*;
 import java.io.*;
 
-public class ClientGreeter {
+public class ClientGreeter
+{
 
-   public static void main(String [] args) {
-	  //1. Create a String for the ip address of the server. 
-	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
-      //2. Create an integer for the server's port number
-      
-      //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
-    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
-         //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
-         //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
-         //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
-         //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
-         //9. Close the client's server object
+    public static void main(String[] args) throws UnknownHostException
+    {
+        //1. Create a String for the ip address of the server.
+        // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
+        String ipAddress = InetAddress.getLocalHost().getHostAddress();
+        //2. Create an integer for the server's port number
+        int serverPortNumber = 8080;
+        //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
+        try
+        {
+            //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
+            Socket socket = new Socket(ipAddress, serverPortNumber);
+            //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
+            dataOutputStream.writeUTF("12345");
+            //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
+            System.out.println(dataInputStream.readUTF());
+            System.out.println("The program has proceeded through step 8 in the recipe.");
+            //9. Close the client's server object
+            socket.close();
 
-   }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            System.out.println("Something went wrong");
+        }
+    }
 }
